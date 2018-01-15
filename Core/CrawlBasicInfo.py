0d4d112b57bd1c9049aac2ba390843ccd68d1f14
stories2 <stories2@naver.com>
2018-01-15 19:05:38 +0900

    CrawlBasicInfo module : crawl company name working

--- Core/CrawlBasicInfo.py
+++ Core/CrawlBasicInfo.py
@@ -10,5 +10,16 @@ class CrawlBasicInfo(object):
         msg = "web driver status: " + crawlerStatus + " url status: " + urlStatus
         LogManager.PrintLogMessage("CrawlBasicInfo", "__init__", msg, DefineManager.LOG_LEVEL_INFO)
 
+    def CrawlCompanyName(self):
+        try:
+            webDriver = self.webCrawler.GetDriver()
+            companyElements = webDriver.find_element_by_class_name("wrap_company")
+            companyName = companyElements.find_element_by_tag_name("a").text
+            LogManager.PrintLogMessage("CrawlBasicInfo", "CrawlCompanyName", "crawl company name successfully: " + companyName, DefineManager.LOG_LEVEL_INFO)
+            return companyName
+        except:
+            LogManager.PrintLogMessage("CrawlBasicInfo", "CrawlCompanyName", "crawl company name failed", DefineManager.LOG_LEVEL_ERROR)
+        return None
+
     def __del__(self):
         return
--- Runner.py
+++ Runner.py
@@ -3,5 +3,5 @@ from Setting import DefineManager
 
 webCrawler = WebCrawler.WebCrawler()
 crawlBasicInfo = CrawlBasicInfo.CrawlBasicInfo(webCrawler, DefineManager.TEST_CRAWL_URL)
-
+crawlBasicInfo.CrawlCompanyName()
 webCrawler.CloseDriver()
