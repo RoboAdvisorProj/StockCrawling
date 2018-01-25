d4b2c7d1ba366f51af6b27ccf5e4d307c3bff2a5
stories2 <stories2@naver.com>
2018-01-25 14:43:08 +0900

    ExportDataManager module : export data as csv now working

--- Runner.py
+++ Runner.py
@@ -6,6 +6,11 @@ companyCodeList = [
     "000660", "009420", "008700", "010140", "005930", "047810", "035720", "081000", "013870", "028050"
 ]
 
+saveKeyList = [
+    "Name", "Code", "Price", "D_PRH", "D_PRL", "Y_PRH", "Y_PRL", "D_IV", "Change", "Value", "Beta", "PER", "PBR", "EPS",
+    "SALEQ2", "SALEQ1", "SALEQ0", "NIQ2", "NIQ1", "NIQ0", "ACT", "DPT", "CAP"
+]
+
 crawlRoutineManager = CrawlRoutineManager.CrawlRoutineManager()
 crawlRoutineManager.OpenWebDriver()
 crawlRoutineManager.SetCrawlCompanyCode(companyCodeList)
@@ -14,4 +19,4 @@ crawlRoutineManager.CloseWebDriver()
 
 exportDataManager = ExportDataManager.ExportDataManager()
 exportDataManager.SetDataArray(crawlRoutineManager.GetCrawlDataArray())
-exportDataManager.ExportDataArrayAsCSV()
+exportDataManager.ExportDataArrayAsCSV("all.csv", saveKeyList)
--- Utils/ExportDataManager.py
+++ Utils/ExportDataManager.py
@@ -1,5 +1,6 @@
 from Setting import DefineManager
 from Utils import LogManager
+import csv
 
 class ExportDataManager(object):
     def __init__(self):
@@ -11,8 +12,24 @@ class ExportDataManager(object):
         LogManager.PrintLogMessage("ExportDataManager", "SetDataArray", "setup data array", DefineManager.LOG_LEVEL_INFO)
         self.dataArray = dataArray
 
-    def ExportDataArrayAsCSV(self):
+    def ExportDataArrayAsCSV(self, fileName = "", saveKeyList = []):
         LogManager.PrintLogMessage("ExportDataManager", "ExportDataArrayAsCSV", "export data array size: " + str(self.dataArray.__len__()), DefineManager.LOG_LEVEL_INFO)
+
+        try:
+            fileManager = open(fileName, "w", encoding="utf-8", newline="")
+            csvWriter = csv.writer(fileManager)
+            for indexOfDataArray in self.dataArray:
+                csvRow = []
+                for key in indexOfDataArray:
+                    if any(key in keyItem for keyItem in saveKeyList) == True:
+                        csvRow.append(indexOfDataArray[key])
+                csvWriter.writerow(csvRow)
+            fileManager.close()
+
+            LogManager.PrintLogMessage("ExportDataManager", "ExportDataArrayAsCSV", "exported done!",
+                                       DefineManager.LOG_LEVEL_INFO)
+        except:
+            LogManager.PrintLogMessage("ExportDataManager", "ExportDataArrayAsCSV", "export failed", DefineManager.LOG_LEVEL_ERROR)
         return
 
     def __del__(self):
